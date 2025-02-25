693b0e3ba42e4aa3cfe8be7afe5361c8964b177b
Lubos Sekerak <lubos.sekerak@webgang.io>
2025-02-25 21:36:13 +0100

    Updated response content

--- src/main/java/com/onsemi/gpt/service/ModelSelectorService.java
+++ src/main/java/com/onsemi/gpt/service/ModelSelectorService.java
@@ -18,27 +18,27 @@ public class ModelSelectorService {
         switch (modelType) {
             case ModelSelectorEnum.SEARCH_PART_BY_PARAMS:
                 response = new GPTResponse<>();
-                response.setContent("Model 1");
+                response.setContent("Kategória 1");
                 return GPTService.getResponse(request);
             case ModelSelectorEnum.SUGGEST_COMPLEMENTARY_PARTS:
                 response = new GPTResponse<>();
-                response.setContent("Model 2");
+                response.setContent("Kategória 2");
                 return response;
             case ModelSelectorEnum.FIND_DOCUMENTATION_FOR_PARTS:
                 response = new GPTResponse<>();
-                response.setContent("Model 3");
+                response.setContent("Kategória 3");
                 return response;
             case ModelSelectorEnum.FIND_SIMILAR_PARTS:
                 response = new GPTResponse<>();
-                response.setContent("Model 4");
+                response.setContent("Kategória 4");
                 return response;
             case ModelSelectorEnum.CHECK_AVAILABILITY_AND_PRICE:
                 response = new GPTResponse<>();
-                response.setContent("Model 5");
+                response.setContent("Kategória 5");
                 return response;
             default:
                 response = new GPTResponse<>();
-                response.setContent("Ziadny model");
+                response.setContent("Žiadna kategória");
                 return response;
         }
     }
