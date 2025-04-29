e6c3cb45c34f8cb349ed49ae9e8fbef4081c5e33
Klára <klara.murinova@gmail.com>
2025-04-29 13:41:04 +0200

    rename categories, add model GPT without regex

--- src/main/java/com/onsemi/gpt/service/ModelSelectorService.java
+++ src/main/java/com/onsemi/gpt/service/ModelSelectorService.java
@@ -12,11 +12,11 @@ import org.springframework.stereotype.Service;
 public class ModelSelectorService {
 
     private GPTService GPTService;
-    private GPTSimilarPartsService GPTSimilarPartsService;
-    private GPTComplementaryPartsService GPTComplementaryPartsService;
+    private SimilarPartsService SimilarPartsService;
+    private ComplementaryPartsService ComplementaryPartsService;
     private GPTCategoryService GPTCategoryService;
     private RegexService regexService;
-    private FindDocsForPartsService findDocsForPartsService;
+    private PartNumberService PartNumberService;
 
     public GPTResponse<?> getResponse(GPTRequest request) throws Exception {
         GPTResponse<?> response;
@@ -26,26 +26,29 @@ public class ModelSelectorService {
         switch (modelType) {
             case ModelSelectorEnum.SEARCH_PART_BY_PARAMS:
                 return GPTService.getResponse(request);
-            case ModelSelectorEnum.SUGGEST_COMPLEMENTARY_PARTS:
-                return GPTComplementaryPartsService.getResponse(selectedProductId);
-            case ModelSelectorEnum.FIND_DOCUMENTATION_FOR_PARTS:
-                return findDocsForPartsService.getResponse(selectedProductId);
-            case ModelSelectorEnum.FIND_SIMILAR_PARTS:
-                return GPTSimilarPartsService.getResponse(selectedProductId);
+            case ModelSelectorEnum.COMPLEMENTARY_PARTS:
+                return ComplementaryPartsService.getResponse(selectedProductId);
+            case ModelSelectorEnum.PART_NUMBER:
+                return PartNumberService.getResponse(selectedProductId);
+            case ModelSelectorEnum.SIMILAR_PARTS:
+                return SimilarPartsService.getResponse(selectedProductId);
             case ModelSelectorEnum.OTHER:
                 response = new GPTResponse<>();
-                response.setContent("I can't answer your request");
+                response.setContent("Thank you for your request. However, it appears that your request does not match any of the standard categories we support (technical details, part numbers, complementary parts, or similar parts).\n" +
+                        "You might want to reframe or clarify your request so we can assist you more effectively within ONSEMI’s product scope.");
                 return response;
             default:
                 response = new GPTResponse<>();
-                response.setContent("I can't answer your request");
+                response.setContent("Thank you for your request. However, it appears that your request does not match any of the standard categories we support (technical details, part numbers, complementary parts, or similar parts).\n" +
+                        "You might want to reframe or clarify your request so we can assist you more effectively within ONSEMI’s product scope.");
                 return response;
         }
     }
 
     public ModelSelectorEnum selectModel(GPTRequest request, String selectedProductId) {
-        return GPTCategoryService.selectModel(request, selectedProductId);
-        // return LuisService.getModelId(request);
-        // return EmbeddingsService.run(request.getRequest());
+        return GPTCategoryService.selectModel(request, selectedProductId); // <- GPT + regex
+        //return GPTCategoryService.selectModel(request);  // <- GPT bez regexu
+        //return LuisService.getModelId(request);
+        //return EmbeddingsService.run(request.getRequest());
     }
 }
